67014506ec4f01b9bf6922d84bb3eab6517de78c
Mauricio Quintanilla <mauricio.q.56@gmail.com>
2020-06-15 23:10:38 +1200

    add working version of step 1

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -24,9 +24,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+    
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello Mauricio!</h1>");
   }
 }
